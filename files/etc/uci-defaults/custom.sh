8a2333209863d968ba7d6fbdb31804bc6f235842
LightBearer <45056654+LighrBearer@users.noreply.github.com>
2025-01-02 17:39:24 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -33,9 +33,6 @@ uci delete ttyd.@ttyd[0].interface
 uci set dropbear.@dropbear[0].Interface=''
 uci commit
 
-# 宽带账号
-pppoe_username="a8736522"
-pppoe_password="a123456"
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
